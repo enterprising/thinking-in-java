427914337f19abc038ce67d234a8a548968c73e8
enterprising <tanpeng_net@163.com>
2017-08-23 20:05:19 +0800

    用信号量实现同步

---
+++ src/aa_jcip/BoundedHashSet.java
@@ -0,0 +1,41 @@
+package aa_jcip;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * BoundedHashSet
+ * <p/>
+ * Using Semaphore to bound a collection
+ * 用信号量来实现同步
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class BoundedHashSet<T> {
+    private final Set<T> set;
+    private final Semaphore sem;
+
+    public BoundedHashSet(int bound) {
+        this.set = Collections.synchronizedSet(new HashSet<T>());
+        sem = new Semaphore(bound);
+    }
+
+    public boolean add(T o) throws InterruptedException {
+        sem.acquire();
+        boolean wasAdded = false;
+        try {
+            wasAdded = set.add(o);
+            return wasAdded;
+        } finally {
+            if (!wasAdded)
+                sem.release();
+        }
+    }
+
+    public boolean remove(Object o) {
+        boolean wasRemoved = set.remove(o);
+        if (wasRemoved)
+            sem.release();
+        return wasRemoved;
+    }
+}
