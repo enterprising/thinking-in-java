2e1826532e20f83c03c92a21d2d3693911b5cb7b
enterprising <tanpeng_net@163.com>
2017-08-31 21:09:08 +0800

    Java注解

--- src/annotations/AtUnitComposition.java
+++ src/annotations/AtUnitComposition.java
@@ -1,22 +1,28 @@
 //: annotations/AtUnitComposition.java
 // Creating non-embedded tests.
 package annotations;
+
 import net.mindview.atunit.*;
 import net.mindview.util.*;
 
 public class AtUnitComposition {
-  AtUnitExample1 testObject = new AtUnitExample1();
-  @Test boolean _methodOne() {
-    return
-      testObject.methodOne().equals("This is methodOne");
-  }
-  @Test boolean _methodTwo() {
-    return testObject.methodTwo() == 2;
-  }
-  public static void main(String[] args) throws Exception {
-    OSExecute.command(
-     "java net.mindview.atunit.AtUnit AtUnitComposition");
-  }
+    AtUnitExample1 testObject = new AtUnitExample1();
+
+    @Test
+    boolean _methodOne() {
+        return
+                testObject.methodOne().equals("This is methodOne");
+    }
+
+    @Test
+    boolean _methodTwo() {
+        return testObject.methodTwo() == 2;
+    }
+
+    public static void main(String[] args) throws Exception {
+        OSExecute.command(
+                "java net.mindview.atunit.AtUnit AtUnitComposition");
+    }
 } /* Output:
 annotations.AtUnitComposition
   . _methodOne
