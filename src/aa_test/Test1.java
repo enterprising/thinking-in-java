fb2b6acfbef401be2171557309c3e4939ec22d15
enterprising <tanpeng_net@163.com>
2017-09-12 19:44:41 +0800

    test

---
+++ src/aa_test/Test1.java
@@ -0,0 +1,21 @@
+package aa_test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by peng.tan on 17/9/6.
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        ArrayList<String> machines = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        list.add("dt.push.org");
+        list.add("dt.push.org.pg");
+        list.add("dt.push.org.sadsd");
+
+    }
+
+
+}
